466760c36c150a2ef0cf119eac6ed201b428c29e
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-09-26 13:05:37 -0300

    Update MSC.R

--- MSC.R
+++ MSC.R
@@ -12,8 +12,9 @@ for (i in 1:length(model_files)){
 
 horizons = c(1,3,6,12)
 errors_list = lapply(horizons, function(h){
-  lapply(models_list, function(x)x[,h]-yout[,1])
+  lapply(models_list, function(x)(x[,h]-yout[,1])^2)
 })
+
 
 
 errors <- lapply(1:length(horizons), function(i) {
@@ -26,31 +27,35 @@ errors_h3 = errors[[2]]
 errors_h6 = errors[[3]]
 errors_h12 = errors[[4]]
 
+View(errors)
+
+View(errors_h1)
+
 
 # aplicando MSC h1
 
-MCSprocedure(errors_h1, alpha = 0.90, B = 10000, cl = NULL,
-             ram.allocation = TRUE, statistic = "Tmax", k = NULL, min.k = 3,
+MCSprocedure(errors_h1, alpha = 0.15, B = 5000, cl = NULL,
+             ram.allocation = TRUE, statistic = "TR", k = NULL, min.k = 3,
              verbose = TRUE)
 
 
 # aplicando MSC h3
 
-MCSprocedure(errors_h3, alpha = 0.90, B = 10000, cl = NULL,
+MCSprocedure(errors_h3, alpha = 0.15, B = 5000, cl = NULL,
              ram.allocation = TRUE, statistic = "Tmax", k = NULL, min.k = 3,
              verbose = TRUE)
 
 
 # aplicando MSC h6
 
-MCSprocedure(errors_h6, alpha = 0.90, B = 10000, cl = NULL,
+MCSprocedure(errors_h6, alpha = 0.15, B = 5000, cl = NULL,
              ram.allocation = TRUE, statistic = "Tmax", k = NULL, min.k = 3,
              verbose = TRUE)
 
 
 # aplicando MSC h12
 
-MCSprocedure(errors_h12, alpha = 0.15, B = 10000, cl = NULL,
+MCSprocedure(errors_h12, alpha = 0.15, B = 5000, cl = NULL,
              ram.allocation = TRUE, statistic = "Tmax", k = NULL, min.k = 3,
              verbose = TRUE)
 
